fd02ef7ff99005f509b15c32712c7cdd7a1cd520
lsljw <lsljw@naver.com>
2019-03-06 19:04:50 +0900

    start

---
+++ phpstart.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>php프로젝트 상수</title>
+
+</head>
+<body>
+<h1>숫자와 문자</h1>
+<h2>1+1</h2>
+<?php
+  echo (1+1)."<br />"."1+1"."<br />";
+  echo "php의 '문자'와 '상수'입니다."."<br />";
+  echo "<h2>".(2*15)."</h2>";
+ ?>
+</body>
+</html>
