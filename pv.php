bedadb446b98bf623b717fc75e6ebb1ee824de20
lsljw <lsljw@naver.com>
2019-03-08 11:52:07 +0900

    변수

---
+++ pv.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>php프로젝트 상수</title>
+
+</head>
+<body>
+<h1>숫자와 문자</h1>
+<h2>1+1</h2>
+<?php
+  $var_num="박만기";
+  $num=3;
+  echo ($num+1)."<br />";
+  echo "php의 '문자'와 '상수'입니다."."<br />";
+  echo "<h2>".($num*15)."</h2>";
+  echo $var_num."는 변수에 들어있는 값입니다.<br />";
+  echo "사람의 이름은 ".$var_num."입니다."
+ ?>
+</body>
+</html>
