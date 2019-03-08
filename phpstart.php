bc466f4ee00ebdbfe307efc30e46c24f24f86f4b
lsljw <lsljw@naver.com>
2019-03-08 12:05:38 +0900

    변수

--- phpstart.php
+++ phpstart.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8" />
-<title>php프로젝트 상수</title>
+<title>php프로젝트 상수의 처리</title>
 
 </head>
 <body>
--- pv.php
+++ pv.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8" />
-<title>php프로젝트 상수</title>
+<title>변수</title>
 
 </head>
 <body>
